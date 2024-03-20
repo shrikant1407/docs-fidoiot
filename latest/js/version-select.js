b5671ec7a26ff466ed4fb998c5c222551e446ef6
sdo-automation <sdo-automation@intel.com>
2024-03-20 06:17:04 +0000

    Deployed dfac251 to latest with MkDocs 1.5.3 and mike 2.0.0

--- latest/js/version-select.js
+++ latest/js/version-select.js
@@ -1,49 +1,71 @@
 window.addEventListener("DOMContentLoaded", function() {
-  // This is a bit hacky. Figure out the base URL from a known CSS file the
-  // template refers to...
-  var ex = new RegExp("/?css/version-select.css$");
-  var sheet = document.querySelector('link[href$="version-select.css"]');
+  function expandPath(path) {
+    // Get the base directory components.
+    var expanded = window.location.pathname.split("/");
+    expanded.pop();
+    var isSubdir = false;
 
-  var ABS_BASE_URL = sheet.href.replace(ex, "");
-  var CURRENT_VERSION = ABS_BASE_URL.split("/").pop();
+    path.split("/").forEach(function(bit, i) {
+      if (bit === "" && i === 0) {
+        isSubdir = false;
+        expanded = [""];
+      } else if (bit === "." || bit === "") {
+        isSubdir = true;
+      } else if (bit === "..") {
+        if (expanded.length === 1) {
+          // We must be trying to .. past the root!
+          throw new Error("invalid path");
+        } else {
+          isSubdir = true;
+          expanded.pop();
+        }
+      } else {
+        isSubdir = false;
+        expanded.push(bit);
+      }
+    });
 
-  function makeSelect(options, selected) {
+    if (isSubdir)
+      expanded.push("");
+    return expanded.join("/");
+  }
+
+  // `base_url` comes from the base.html template for this theme.
+  var ABS_BASE_URL = expandPath(base_url);
+  var CURRENT_VERSION = ABS_BASE_URL.match(/\/([^\/]+)\/$/)[1];
+
+  function makeSelect(options) {
     var select = document.createElement("select");
-    select.classList.add("form-control");
 
     options.forEach(function(i) {
       var option = new Option(i.text, i.value, undefined,
-                              i.value === selected);
+                              i.selected);
       select.add(option);
     });
 
     return select;
   }
 
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", ABS_BASE_URL + "/../versions.json");
-  xhr.onload = function() {
-    var versions = JSON.parse(this.responseText);
-
+  fetch(ABS_BASE_URL + "../versions.json").then((response) => {
+    return response.json();
+  }).then((versions) => {
     var realVersion = versions.find(function(i) {
       return i.version === CURRENT_VERSION ||
              i.aliases.includes(CURRENT_VERSION);
     }).version;
 
-    var select = makeSelect(versions.map(function(i) {
-      return {text: i.title, value: i.version};
-    }), realVersion);
+    var select = makeSelect(versions.filter(function(i) {
+      return i.version === realVersion || !i.properties || !i.properties.hidden;
+    }).map(function(i) {
+      return {text: i.title, value: i.version,
+              selected: i.version === realVersion};
+    }));
+    select.id = "version-selector";
     select.addEventListener("change", function(event) {
-      window.location.href = ABS_BASE_URL + "/../" + this.value;
+      window.location.href = ABS_BASE_URL + "../" + this.value + "/";
     });
 
-    var container = document.createElement("div");
-    container.id = "version-selector";
-    container.className = "md-nav__item";
-    container.appendChild(select);
-
-    var sidebar = document.querySelector(".md-nav--primary > .md-nav__list");
-    sidebar.parentNode.insertBefore(container, sidebar);
-  };
-  xhr.send();
+    var title = document.querySelector("div.wy-side-nav-search");
+    title.insertBefore(select, title.querySelector(".icon-home").nextSibling);
+  });
 });
